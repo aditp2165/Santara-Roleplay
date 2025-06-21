602dce1f7d50bb4276ce319bae930f33c40e5414
Aditya Pratama <aditp2165@gmail.com>
2025-06-21 21:03:14 +0700

    add netlify build config

---
+++ install-flutter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
+export PATH="$PATH:`pwd`/flutter/bin"
+flutter doctor
