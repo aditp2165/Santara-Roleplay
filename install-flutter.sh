603811ea809018c9de48c57120de72b2c6413513
Aditya Pratama <aditp2165@gmail.com>
2025-06-21 21:09:04 +0700

    Add install-flutter.sh for Netlify deploy

--- install-flutter.sh
+++ install-flutter.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+
+# Download Flutter SDK
 wget https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
 tar xf flutter_linux_${FLUTTER_VERSION}-stable.tar.xz
-export PATH="$PATH:`pwd`/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# Cek instalasi Flutter
 flutter doctor
